af243b350974f96f6ace58e647d5f34bfe31e017
heejoo Jeong <64790176+heeheejj@users.noreply.github.com>
2022-06-12 05:49:01 +0000

    2022 SK텔레콤 T-WorX for Developers 챌린지 코딩테스트 4번

---
+++ gichool/2022_06_skt_intern/4.py
@@ -0,0 +1,21 @@
+# 풀다 말음
+def dfs(grid, x, y):
+    if x < 0 or x >= n or y < 0 or y >= m:
+        return
+    if grid[x][y] == "#":
+        return
+    else:   # 이동 가능
+        dfs(x+1, y)
+        dfs(x-1, y)
+        dfs(x, y+1)
+        dfs(x, y-1)
+
+dx = [0, 1 , 0, -1]
+dy = [1, 0, -1, 0]
+n, m = 0, 0
+def solution(grid, k):
+    answer = -1
+    n = len(grid)
+    m = len(grid[0])
+    dfs(grid, 0,0)
+    return answer
