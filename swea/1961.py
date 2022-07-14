6286def445aea464e27eff4e0eb00bd51fb2f378
heejoo Jeong <64790176+heeheejj@users.noreply.github.com>
2022-07-14 14:23:23 +0000

    1961: 숫자 배열 회전

--- swea/1961.py
+++ swea/1961.py
@@ -1,12 +1,26 @@
 # 숫자 배열 회전
 
-import sys
-sys.stdin = open("input.txt", "r")
+# import sys
+
+# sys.stdin = open("input.txt", "r")
 T = int(input())
+
+def rotate(a, N):  # 2차원배열을 90도 돌리는 함수
+  _a = [[0] * N for _ in range(N)]
+  for i in range(N):
+    for j in range(N):
+      _a[i][j] = a[N-j-1][i]
+  return _a
 
 for t in range(1, T+1):
   print("#"+str(t))
   N = int(input())
-  arr = [list(map(int, input().split())) for _ in range(N)]
-
-  
+  arr = [list(input().split()) for _ in range(N)]
+  result1 = rotate(arr, N)
+  result2 = rotate(result1, N)
+  result3 = rotate(result2, N)
+  for i in range(N):
+    print("".join(result1[i]), end=' ')
+    print("".join(result2[i]), end=' ')
+    print("".join(result3[i]), end=' ')
+    print()
