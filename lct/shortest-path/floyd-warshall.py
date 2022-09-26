452ac61dd5b1c49fd8e84c53eb19ed115a193f38
heejoo Jeong <64790176+heeheejj@users.noreply.github.com>
2022-09-26 12:41:17 +0000

    1952: 수영장

--- lct/shortest-path/floyd-warshall.py
+++ lct/shortest-path/floyd-warshall.py
@@ -13,23 +13,23 @@ m = int(input())
 graph = [[INF] * (n + 1) for _ in range(n + 1)]
 
 for i in range(1, n + 1):
-  for j in range(1, n + 1):
-    if i == j:
-      graph[i][j] = 0
+    for j in range(1, n + 1):
+        if i == j:
+            graph[i][j] = 0
 
 for i in range(m):
-  a, b, c = map(int, input().rstrip().split())
-  graph[a][b] = c
-
-for a in range(n + 1):
-  for b in range(n + 1):
-    for k in range(n + 1):
-      graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+    a, b, c = map(int, input().rstrip().split())
+    graph[a][b] = c
 
 for a in range(1, n + 1):
-  for b in range(1, n + 1):
-    if graph[a][b] == INF:
-      print("INFINITY", end = ' ')
-    else:
-      print(graph[a][b], end = ' ')
-  print("")
+    for b in range(1, n + 1):
+        for k in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if graph[a][b] == INF:
+            print("INFINITY", end=' ')
+        else:
+            print(graph[a][b], end=' ')
+    print("")
